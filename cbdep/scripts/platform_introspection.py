a8127d6849e3bb43591ca88b3032e0d7456c3f64
Chris Hillery <ceej@couchbase.com>
2019-04-30 02:14:19 -0700

    CBD-2939: Support for Oracle JDK 11
    Change-Id: I1a2b6be667a07717069d1530f26ece024cec180d
    Reviewed-on: http://review.couchbase.org/108431
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- cbdep/scripts/platform_introspection.py
+++ cbdep/scripts/platform_introspection.py
@@ -42,6 +42,7 @@ def get_platforms():
         platforms.insert(0, "macosx")
         platforms.insert(0, "macos")
         platforms.insert(0, "mac")
+        platforms.insert(0, "osx")
 
     elif system == "windows":
         # QQQ Somehow introspect MSVC version?
